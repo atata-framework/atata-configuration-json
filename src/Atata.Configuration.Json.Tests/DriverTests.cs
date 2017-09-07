c8252c60fc26f2b9065512c18ed8473c3ee758e6
YevgeniyShunevych <eshunevich@gmail.com>
2017-09-07 11:44:06 +0300

    Update Driver_Firefox test; Add Selenium.Firefox.WebDriver package

--- src/Atata.Configuration.Json.Tests/DriverTests.cs
+++ src/Atata.Configuration.Json.Tests/DriverTests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using FluentAssertions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 
 namespace Atata.Configuration.Json.Tests
@@ -19,12 +21,13 @@ namespace Atata.Configuration.Json.Tests
             using (context.UseNullDriver())
             {
                 AtataContextBuilder builder = AtataContext.Build().
-                    ApplyJsonConfig(@"Configs\Chrome.json");
+                    ApplyJsonConfig(@"Configs/Chrome.json");
 
                 builder.BuildingContext.DriverCreator();
             }
 
             var capabilities = context.Options.ToCapabilities();
+            var optionsCapabilities = (Dictionary<string, object>)capabilities.GetCapability(ChromeOptions.Capability);
 
             capabilities.GetCapability(CapabilityType.LoggingPreferences).ShouldBeEquivalentTo(new Dictionary<string, object>
             {
@@ -32,9 +35,7 @@ namespace Atata.Configuration.Json.Tests
                 ["driver"] = "WARNING"
             });
 
-            var chromeCapabilities = (Dictionary<string, object>)capabilities.GetCapability(ChromeOptions.Capability);
-
-            chromeCapabilities.Should().Contain(new Dictionary<string, object>
+            optionsCapabilities.Should().Contain(new Dictionary<string, object>
             {
                 ["cap1"] = true,
                 ["cap2"] = 5,
@@ -51,32 +52,32 @@ namespace Atata.Configuration.Json.Tests
 
             context.Options.Arguments.Should().Equal("disable-extensions", "no-sandbox", "start-maximized");
 
-            ((List<string>)chromeCapabilities["excludeSwitches"]).Should().Equal("exc-arg");
+            ((List<string>)optionsCapabilities["excludeSwitches"]).Should().Equal("exc-arg");
 
             context.Options.Extensions.Should().Equal("ZW5jLWV4dDE=", "ZW5jLWV4dDI=");
 
-            ((List<string>)chromeCapabilities["windowTypes"]).Should().Equal("win1", "win2");
+            ((List<string>)optionsCapabilities["windowTypes"]).Should().Equal("win1", "win2");
 
             context.Options.PerformanceLoggingPreferences.IsCollectingPageEvents.Should().BeFalse();
             context.Options.PerformanceLoggingPreferences.IsCollectingTimelineEvents.Should().BeFalse();
             context.Options.PerformanceLoggingPreferences.BufferUsageReportingInterval.Should().Be(TimeSpan.FromSeconds(70));
             context.Options.PerformanceLoggingPreferences.TracingCategories.Should().Be("cat1,cat2");
 
-            ((Dictionary<string, object>)chromeCapabilities["prefs"]).Should().Equal(new Dictionary<string, object>
+            ((Dictionary<string, object>)optionsCapabilities["prefs"]).Should().Equal(new Dictionary<string, object>
             {
                 ["pref1"] = 7,
                 ["pref2"] = false,
                 ["pref3"] = "str"
             });
 
-            ((Dictionary<string, object>)chromeCapabilities["localState"]).Should().Equal(new Dictionary<string, object>
+            ((Dictionary<string, object>)optionsCapabilities["localState"]).Should().Equal(new Dictionary<string, object>
             {
                 ["pref1"] = 2.7,
                 ["pref2"] = true,
                 ["pref3"] = string.Empty
             });
 
-            ((Dictionary<string, object>)chromeCapabilities["mobileEmulation"]).Should().Equal(new Dictionary<string, object>
+            ((Dictionary<string, object>)optionsCapabilities["mobileEmulation"]).Should().Equal(new Dictionary<string, object>
             {
                 ["deviceName"] = "emul"
             });
@@ -93,12 +94,45 @@ namespace Atata.Configuration.Json.Tests
             using (context.UseNullDriver())
             {
                 AtataContextBuilder builder = AtataContext.Build().
-                    ApplyJsonConfig(@"Configs\Firefox.json");
+                    ApplyJsonConfig(@"Configs/Firefox.json");
 
                 builder.BuildingContext.DriverCreator();
             }
 
             var capabilities = context.Options.ToCapabilities();
+            var optionsCapabilities = (Dictionary<string, object>)capabilities.GetCapability("moz:firefoxOptions");
+
+            optionsCapabilities.Should().Contain(new Dictionary<string, object>
+            {
+                ["cap1"] = true,
+                ["cap2"] = 5,
+                ["cap3"] = "str"
+            });
+
+            capabilities.GetCapability("globalcap1").Should().Be(true);
+            capabilities.GetCapability("globalcap2").Should().Be(5);
+            capabilities.GetCapability("globalcap3").Should().Be("str");
+
+            context.Options.Proxy.Kind.Should().Be(ProxyKind.ProxyAutoConfigure);
+
+            ((List<object>)optionsCapabilities["args"]).Should().Equal("--start-maximized");
+
+            typeof(FirefoxProfile).GetField("sourceProfileDir", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.GetValue(context.Options.Profile).Should().Be("dir");
+
+            typeof(FirefoxProfile).GetField("deleteSource", BindingFlags.NonPublic | BindingFlags.Instance)
+                ?.GetValue(context.Options.Profile).Should().Be(true);
+
+            context.Options.Profile.EnableNativeEvents.Should().BeTrue();
+
+            ((Dictionary<string, object>)optionsCapabilities["prefs"]).Should().Equal(new Dictionary<string, object>
+            {
+                ["pref1"] = true,
+                ["pref2"] = 5,
+                ["pref3"] = "str"
+            });
+
+            context.Options.LogLevel.Should().Be(FirefoxDriverLogLevel.Warn);
         }
     }
 }

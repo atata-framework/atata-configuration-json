fae7fb4d183cef200bd20d947e5064bf46ece195
YevgeniyShunevych <eshunevich@gmail.com>
2017-09-11 11:10:13 +0300

    Add GeneralSettings_NUnit test

---
+++ src/Atata.Configuration.Json.Tests/GeneralSettingsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atata.Configuration.Json.Tests
+{
+    [TestFixture]
+    public class GeneralSettingsTests
+    {
+        [Test]
+        public void GeneralSettings_NUnit()
+        {
+            AtataContextBuilder builder = AtataContext.Build().
+                ApplyJsonConfig("Configs/Chrome+NUnit.json");
+
+            var context = builder.BuildingContext;
+
+            context.BaseUrl.Should().Be("https://atata-framework.github.io/atata-sample-app/#!/");
+
+            context.CleanUpActions.Should().HaveCount(2);
+
+            context.AssertionExceptionType.Should().Be(typeof(NUnit.Framework.AssertionException));
+
+            context.RetryTimeout.Should().Be(TimeSpan.FromSeconds(7));
+            context.RetryInterval.Should().Be(TimeSpan.FromSeconds(0.7));
+
+            context.TestNameFactory().Should().Be(nameof(GeneralSettings_NUnit));
+        }
+    }
+}

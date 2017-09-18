f5540c63d003369f6f6b50638c1a203c387964cc
YevgeniyShunevych <eshunevich@gmail.com>
2017-09-18 15:16:13 +0300

    Add CustomSettings test

---
+++ src/Atata.Configuration.Json.Tests/CustomJsonConfig.cs
@@ -0,0 +1,33 @@
+﻿namespace Atata.Configuration.Json.Tests
+{
+    public class CustomJsonConfig : JsonConfig<CustomJsonConfig>
+    {
+        public int IntProperty { get; set; }
+
+        public string StringProperty { get; set; }
+
+        public bool BoolProperty { get; set; }
+
+        public bool BoolProperty2 { get; set; }
+
+        public string[] StringArrayValues { get; set; }
+
+        public CustomSection Section { get; set; }
+
+        public CustomItemSection[] Items { get; set; }
+
+        public class CustomSection
+        {
+            public string StringProperty { get; set; }
+
+            public bool BoolProperty { get; set; }
+        }
+
+        public class CustomItemSection
+        {
+            public string Name { get; set; }
+
+            public int Value { get; set; }
+        }
+    }
+}
---
+++ src/Atata.Configuration.Json.Tests/CustomSettingsTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Atata.Configuration.Json.Tests
+{
+    [TestFixture]
+    public class CustomSettingsTests
+    {
+        [Test]
+        public void CustomSettings()
+        {
+            AtataContext.Build().
+                ApplyJsonConfig<CustomJsonConfig>(@"Configs/CustomSettings.json");
+
+            CustomJsonConfig.Current.IntProperty.Should().Be(5);
+            CustomJsonConfig.Current.StringProperty.Should().Be("str");
+            CustomJsonConfig.Current.BoolProperty.Should().Be(true);
+            CustomJsonConfig.Current.BoolProperty2.Should().Be(false);
+            CustomJsonConfig.Current.StringArrayValues.Should().Equal(new[] { "str1", "str2", "str3" });
+
+            CustomJsonConfig.Current.Section.StringProperty.Should().Be("section_str");
+            CustomJsonConfig.Current.Section.BoolProperty.Should().Be(true);
+
+            CustomJsonConfig.Current.Items.ShouldBeEquivalentTo(new[]
+            {
+                new CustomJsonConfig.CustomItemSection
+                {
+                    Name = "item1",
+                    Value = 5
+                },
+                new CustomJsonConfig.CustomItemSection
+                {
+                    Name = "item2",
+                    Value = 7
+                }
+            });
+        }
+    }
+}

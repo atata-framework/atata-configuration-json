2b842745bef9840c40b517db654490317b1b3577
YevgeniyShunevych <eshunevich@gmail.com>
2017-10-26 17:15:00 +0300

    Update Atata.Configuration.Json project to support .NET 4.0, .NET 4.5 and .NET Standard 2.0

--- src/Atata.Configuration.Json/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Atata.Configuration.Json")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("d58a65c7-b2d0-4c1e-b97c-c31a6833ae35")]
